5e401ce10cc449d96ae73ecbf7dcbecedb3aad10
vkadi-budetak <126949497+vkadi-budetak@users.noreply.github.com>
2025-03-02 21:19:55 +0100

    test

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
-
-
 const ulEl = document.querySelector("#categories");
 const ulElChilds = ulEl.querySelectorAll(".item");
 
-console.log('Number of categories:', ulElChilds.length);
+console.log("Number of categories:", ulElChilds.length);
 
 ulElChilds.forEach((item) => {
-console.log('Category:', item.firstElementChild.innerText)
-console.log('Elements:', item.lastElementChild.children.length)
+  console.log("Category:", item.firstElementChild.innerText);
+  console.log("Elements:", item.lastElementChild.children.length);
 });
+
+// console.dir(ulElChilds);
